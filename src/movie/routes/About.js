c61479b20013f4bb315f7890eed5d9b235cab0ff
jinss5 <jinss1145@gmail.com>
2021-07-15 12:35:12 +0900

    updated about page

--- src/movie/routes/About.js
+++ src/movie/routes/About.js
@@ -4,11 +4,14 @@ import "./About.css";
 function About() {
     return (
         <div className="about__container">
-            <span>My first JS project using React.js. Front-end</span>
-            <li>Developed a movie web service that exhibits features such as title, summary, and poster using React.js</li>
-            <li>Utilized Axios to fetch data from yts-proxy API</li>
-            <li>Implemented React Router to navigate through different pages without refreshing</li>
-            <li>Uploaded application on GitHub</li>
+            <span>Movie Service App</span><br/>
+            <span>Front-end developer</span>
+            <ul>
+                <li>Developed a movie web service that exhibits features such as title, summary, and poster using React.js</li>
+                <li>Utilized Axios to fetch data from yts-proxy API</li>
+                <li>Implemented React Router to navigate through different pages without refreshing</li>
+                <li>Uploaded application on GitHub</li>
+            </ul>
         </div>
     );
 }
